40185ce7fbdaba15b64ef8660f929480a6e4f08b
Malik Haziq <malikhaziq153@gmail.com>
2025-08-09 16:13:50 +0500

    implement new order functionality

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 export default function Header() {
-  return <Link to="/">React fast Pizza Co.</Link>;
+  return (
+    <header>
+      <Link to="/">React fast Pizza Co.</Link>
+      <SearchOrder />
+    </header>
+  );
 }
