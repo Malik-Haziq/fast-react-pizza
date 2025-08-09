f45d3fdae513919be4eba2035944b5fbc4078fdc
Malik Haziq <malikhaziq153@gmail.com>
2025-08-09 16:59:44 +0500

    add tailwind styles

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,22 +1,22 @@
-import { useLoaderData } from "react-router-dom";
-import { getMenu } from "../../services/apiRestaurant";
-import MenuItem from "./MenuItem";
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
 
 function Menu() {
   const menu = useLoaderData();
-  console.log(menu);
 
   return (
-    <div>
+    <ul className="divide-y divide-stone-200 px-2">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
-    </div>
+    </ul>
   );
 }
 
 export async function loader() {
-  return await getMenu();
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
